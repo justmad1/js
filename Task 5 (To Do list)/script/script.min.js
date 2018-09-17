4be4b4cd85551b1111eeee40bcaacffcc2a7bb75
Dmitry Kaspiarovich <claywhoami@yandex.ru>
2018-09-18 00:26:21 +0300

    task 5

---
+++ Task 5 (To Do list)/script/script.min.js
@@ -0,0 +1,118 @@
+let inp = document.querySelector('.inp');
+let ul = document.querySelector('.list');
+let add = document.querySelector('.add');
+let clear = document.querySelectorAll('.clear');
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const storageData = JSON.parse(localStorage.getItem('items'));
+let itemsCount = 0;
+
+const liMaker = (data) => {
+    const li = document.createElement('li');
+    const cb = document.createElement('input');
+    const text = document.createElement('span');
+    const close = document.createElement('span');
+    const edit = document.createElement('span');
+    cb.type = "checkbox";
+    text.innerHTML = data;
+    text.classList.add("text");
+    inp.value = "";
+    li.className = "item";
+    close.innerHTML = "X";
+    close.className = "item-button close";
+    edit.className = "item-button edit";
+    edit.innerHTML = "edit";
+    close.addEventListener('click', () => {
+        if (confirm("Remove this item?")) {
+            li.remove();
+            itemsCount--;
+            check();
+            itemsArray.splice(itemsArray.indexOf(data), 1);
+            localStorage.setItem('items', JSON.stringify(itemsArray));
+        }
+    });
+    edit.addEventListener('click', () => {
+        text.innerHTML = prompt("Edit text here", text.innerHTML);
+        itemsArray.splice(itemsArray.indexOf(data), 1, text.innerHTML);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+    });
+    li.addEventListener('click', (event) => {
+        if (event.toElement == li || event.toElement == cb) {
+            if (li.classList.length > 1) {
+                li.classList.remove("checked");
+                li.firstChild.checked = false;
+            } else {
+                li.classList.add("checked");
+                li.firstChild.checked = true;
+            }
+        }
+    });
+    li.appendChild(cb);
+    li.appendChild(text);
+    li.appendChild(close);
+    li.appendChild(edit);
+    ul.appendChild(li);
+    itemsCount++;
+    check();
+    add.disabled = true;
+}
+
+function check() {
+    if (!itemsCount) {
+        clear.forEach((elem) => {
+            elem.style.display = "none";
+        });
+    } else {
+        clear.forEach((elem) => {
+            elem.style.display = "inline";
+        });
+    }
+}
+
+inp.addEventListener('keyup', () => {
+    if (!inp.value) {
+        add.disabled = true;
+    } else {
+        add.disabled = false;
+    }
+});
+
+add.addEventListener('click', () => {
+    if (inp.value) {
+        itemsArray.push(inp.value);
+        liMaker(inp.value);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        inp.value = "";
+    }
+});
+
+clear[0].addEventListener('click', () => {
+    if (confirm("Clear all items?")) {
+        ul.innerHTML = "";
+        itemsCount = 0;
+        check();
+        localStorage.clear();
+        itemsArray.splice(0, itemsArray.length);
+    }
+});
+
+clear[1].addEventListener('click', () => {
+    if (confirm("Clear selected items?")) {
+        itemsArray.splice(0, itemsArray.length);
+        document.querySelectorAll('.item').forEach((elem) => {
+            if (elem.firstChild.checked) {
+                elem.remove();
+                itemsCount--;
+            } else {
+                itemsArray.push(elem.children[1].innerHTML);
+            }
+        });
+        check();
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+    }
+});
+
+storageData.forEach( item => {
+    liMaker(item);
+});
+
